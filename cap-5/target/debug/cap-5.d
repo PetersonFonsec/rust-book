78a528606ab6a246f5b2b7612233ccf2f2110897
peterson <petersonsimiao1@gmail.com>
2025-01-12 13:04:52 -0300

    cap 5: Brincanddo um pouco com structs

---
+++ cap-5/target/debug/cap-5.d
@@ -0,0 +1,1 @@
+C:\Users\peter\Downloads\projetos\rust\rust-book\cap-5\target\debug\cap-5.exe: C:\Users\peter\Downloads\projetos\rust\rust-book\cap-5\src\main.rs
---
+++ cap-5/target/debug/deps/cap_5.d
@@ -0,0 +1,5 @@
+C:\Users\peter\Downloads\projetos\rust\rust-book\cap-5\target\debug\deps\cap_5.exe: src/main.rs
+
+C:\Users\peter\Downloads\projetos\rust\rust-book\cap-5\target\debug\deps\cap_5.d: src/main.rs
+
+src/main.rs:
